231ecd8577031d4598f6903eea1c83b77604de44
Alex Ribeiro <92212808+alocss@users.noreply.github.com>
2025-03-02 19:22:13 +0000

    OperadorTernario

---
+++ operadorTernario.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main(){
+    int temperatura = 28;
+    int resultado;
+
+
+    temperatura > 30 ? 1 : 0 ;
+
+    if (resultado == 1)
+    {
+        printf ("Está calor !\n");
+    } else{
+        printf(" Está frio ! \n");
+    }
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+/*int main (){
+    int idade = 16; 
+    int resultado;
+
+//condicao ? verdadeiro : falso;
+
+if (condicao)
+{
+    verdadeiro
+} else {
+    falso
+}
+
+resultado = idade >= 18 ? 1 : 0 ;
+
+if (resultado == 1)
+{
+    printf("Você é maior de idade!\n");
+} else{
+    printf("Você é menor de idade!\n");
+}
+
+}
+*/
