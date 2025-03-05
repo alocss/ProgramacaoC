142859a1d403055589fd65da662ed88aad999f19
Alex Ribeiro <92212808+alocss@users.noreply.github.com>
2025-03-05 13:34:59 +0000

    do_while

---
+++ do_while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    int i = 1;
+
+    do {//Entrada (1x pelo menos)
+        printf("%d\n", i);
+        i++;
+         
+    }while (i <= 5);
+    {
+        /* code */
+    }
+    
+  
+    return 0;
+}
