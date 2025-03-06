7d6f31c6b1233f3e21f65fd3118f1ca4a8287f69
Alex Ribeiro <92212808+alocss@users.noreply.github.com>
2025-03-06 13:12:29 +0000

    Loops

--- loopAninhados.c
+++ loopAninhados.c
@@ -40,11 +40,11 @@ int main (){
 
 // Exemplo com "For"
 int main() {
-    for (int i = 1; i <= 10; i++) {  // Loop externo
-        for (int j = 1; j <= 10; j++) {  // Loop interno
-            printf("%d\t", i * j);  // Imprime o produto de i e j
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            printf("%d\t", i * j);
         }
-        printf("\n");  // Nova linha após cada linha da tabela
+        printf("\n");
     }
     return 0;
 }
