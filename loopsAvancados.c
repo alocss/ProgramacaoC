b8c1edcb2743de0338e542c93cd4b928a66f1be3
Alex Ribeiro <92212808+alocss@users.noreply.github.com>
2025-03-07 13:50:49 +0000

    Loops Avançados

---
+++ loopsAvancados.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    // i e j iniciados ao mesmo tempo 
+    // ++ ou --
+    for (  int i = 0, j = 10; i < j; i++, j--)
+    {
+        printf("i = %d, j = %d\n", i,j);
+    }
+    return 0;
+}
+
+ 
+int main() {
+    for (int i = 0, j = 10; i < 5 && j > 5; i++, j--) {
+        printf("i = %d, j = %d\n", i, j);
+    }
+    return 0;
+}
+
+int main() {
+    for (int i = 0; i < 100; i += (i % 2 == 0) ? 1 : 2) {
+        printf("%d ", i);
+    }
+    return 0;
+}
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        if (i == 5) continue; // Pula a iteração quando i é 5
+        if (i == 8) break;    // Sai do loop quando i é 8
+        printf("%d ", i);
+    }
+    return 0;
+}
